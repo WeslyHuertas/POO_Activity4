072d9b53d0c6f8fc7d45ab9224b0bf69a64a63da
WeslyHuertas <126303731+WeslyHuertas@users.noreply.github.com>
2025-01-30 22:36:13 -0500

    Add Activity4

---
+++ PruebaCuenta/src/main/java/com/mycompany/pruebacuenta/CuentaAhorros.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebacuenta;
+
+/**
+ *
+ * @author Wesly
+ */
+
+public class CuentaAhorros extends Cuenta {
+    private boolean activa;
+
+    public CuentaAhorros(float saldo, float tasa) {
+        super(saldo, tasa);
+        activa = saldo >= 10000;
+    }
+
+    @Override
+    public void retirar(float cantidad) {
+        if (activa) {
+            super.retirar(cantidad);
+        }
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if (activa) {
+            super.consignar(cantidad);
+        }
+    }
+
+    @Override
+    public void extractoMensual() {
+        if (numeroRetiros > 4) {
+            comisionMensual += (numeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        activa = saldo >= 10000;
+    }
+
+    public void imprimir() {
+        System.out.println("Saldo = $" + saldo);
+        System.out.println("Comisión mensual = $" + comisionMensual);
+        System.out.println("Número de transacciones = " + (numeroConsignaciones + numeroRetiros));
+        System.out.println();
+    }
+}
+
